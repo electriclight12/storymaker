3f147e13c1b4e41c946f380d91378fa663d6d684
electriclight12 <83523323+electriclight12@users.noreply.github.com>
2022-02-03 08:58:45 -0600

    code

---
+++ storymaker.py
@@ -0,0 +1,10 @@
+print("Welcome to the Python 3 Storymaker!")
+person_thing = input("Start by putting a person or thing...")
+print("Great!")
+setting = input("Now put a setting, where the story will take place...")
+antagonist = input("Ok, so you've chosen a person or thing, and a setting, but now we need a conflict. Choose an antagonist...")
+print("Great job, you have chosen everything, for the story. Now you just need to let the program make it. Please hold.")
+
+print("There once was a " + person_thing + " who lived in a " + setting + ". " + person_thing + " lived a good life, that is until " + antagonist + " came along. Now " + person_thing + " must get rid of " + antagonist + " to return to a normal life.")
+
+input("Great story, thanks for using me! Press any key to close...")
